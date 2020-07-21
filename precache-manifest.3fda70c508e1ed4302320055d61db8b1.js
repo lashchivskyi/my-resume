2c45fe217304e91f726c5f5190fe1ffe675030b9
serhii <lashchivskyi@gmail.com>
2020-07-21 18:44:13 +0300

    Updates

---
+++ precache-manifest.3fda70c508e1ed4302320055d61db8b1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9688ff26b3f99c17f80d89d8adbc3a",
+    "url": "/lashchivskyi/my-resume.git/index.html"
+  },
+  {
+    "revision": "923c94308ecc5fdcb209",
+    "url": "/lashchivskyi/my-resume.git/static/css/main.d08d6079.chunk.css"
+  },
+  {
+    "revision": "8e8b73fc5f90eb89b949",
+    "url": "/lashchivskyi/my-resume.git/static/js/2.aea446bb.chunk.js"
+  },
+  {
+    "revision": "dc326f045be3547e7c497843117df6f8",
+    "url": "/lashchivskyi/my-resume.git/static/js/2.aea446bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923c94308ecc5fdcb209",
+    "url": "/lashchivskyi/my-resume.git/static/js/main.dfe13bd4.chunk.js"
+  },
+  {
+    "revision": "64447811ea9e55287a77",
+    "url": "/lashchivskyi/my-resume.git/static/js/runtime-main.87a5d05f.js"
+  },
+  {
+    "revision": "1f7bf096d91fb55953ad1dd19e97f7fb",
+    "url": "/lashchivskyi/my-resume.git/static/media/Irma.1f7bf096.png"
+  },
+  {
+    "revision": "44c2c3a953b48a4be9bd809e07246e45",
+    "url": "/lashchivskyi/my-resume.git/static/media/crepperie.44c2c3a9.jpg"
+  },
+  {
+    "revision": "e44fcbfa199cde0767de7df8c59d19bf",
+    "url": "/lashchivskyi/my-resume.git/static/media/istudio.e44fcbfa.jpg"
+  },
+  {
+    "revision": "e42edd2fece1960fb39162d06dc61314",
+    "url": "/lashchivskyi/my-resume.git/static/media/itea.e42edd2f.png"
+  },
+  {
+    "revision": "7ca3ddffc3c05e465a5e7aacc20e1674",
+    "url": "/lashchivskyi/my-resume.git/static/media/liet.7ca3ddff.jpg"
+  },
+  {
+    "revision": "c744e9a839c8c2339d47cd935f6a9540",
+    "url": "/lashchivskyi/my-resume.git/static/media/myFoto.c744e9a8.jpg"
+  },
+  {
+    "revision": "3bc77c6934cd5e5591c2b020b1856f95",
+    "url": "/lashchivskyi/my-resume.git/static/media/skill.3bc77c69.jpg"
+  },
+  {
+    "revision": "971abecadcbf633cbdc7b87cfc63edbf",
+    "url": "/lashchivskyi/my-resume.git/static/media/softServe.971abeca.jpg"
+  },
+  {
+    "revision": "1e44bf44f5e947bfc646021760f94d59",
+    "url": "/lashchivskyi/my-resume.git/static/media/website.1e44bf44.png"
+  }
+]);
